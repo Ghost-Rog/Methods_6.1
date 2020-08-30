f6e5e1a3a5cf85260d71e2abdb6f30037bc2d6cd
Ghost-Rog <ghostrog@mail.ru>
2020-08-30 20:54:19 +0300

    Initial commit

---
+++ src/Task01.java
@@ -0,0 +1,31 @@
+public class Task01 {
+
+    public static void creatingAUnit(int [][] array){
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int [][] array = new int[3][3];
+        creatingAUnit(array);
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                array[i][j] = i * j + 1;
+//            }
+//        }
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                System.out.print(" " + array[i][j]);
+//            }
+//            System.out.print("\n");
+//        }
+    }
+}
+
